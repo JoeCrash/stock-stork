831f482a1bbfd60ed155ab57c44c8cc686151288
JoeCrash <joeycrash135@gmail.com>
2025-10-05 03:03:11 -0400

    add header, nav, and dropdown for root

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,19 @@
-import Link from "next/link"
-import Image from "next/image"
-
+import Link from "next/link";
+import Image from "next/image";
+import NavItems from "@/components/NavItems";
+import UserDropdown from "@/components/UserDropdown";
 
 const Header = () => {
   return (
     <header className="sticky top-0 header">
         <div className="container header-wrapper">
-            <Link href="/">
+            <Link href="/public">
                 <Image src="/assets/icons/logo.svg" alt="Stock-Stork Logo" width={140} height={32} className="h-8 w-auto cursor-pointer" />
             </Link>
             <nav className="hidden sm:block">
-                NavItems
+                <NavItems />
             </nav>
-            { /*UserDropDown*/ }
+            <UserDropdown />
         </div>
     </header>
   )
